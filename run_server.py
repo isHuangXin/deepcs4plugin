98dae4d816ca93a74d181d54777f3f05ced20429
Lintian Tao <wojiaowenzhong233@outlook.com>
2022-07-31 19:33:24 +0800

    start a http server for vscode plugin

---
+++ run_server.py
@@ -0,0 +1,46 @@
+import http
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+
+import json
+
+from search import *
+
+class NaccCodeSearchHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = self.headers["query"]
+        n_results = 10
+        query = query.lower().replace('how to ', '').replace('how do i ', '').replace('how can i ', '').replace('?', '').strip()
+        results = search(config, model, vocab_desc, query, n_results)
+        results = sorted(results, reverse=True, key=lambda x:x[1])
+        results = postproc(results)
+        results = list(results)[:n_results]
+        super().send_response(http.HTTPStatus.OK)
+        super().end_headers()
+        self.wfile.write(json.dumps(results).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    args = parse_args()
+    device = torch.device(f"cuda:{args.gpu_id}" if torch.cuda.is_available() else "cpu")
+    config = getattr(configs, 'config_'+args.model)()
+    
+    ##### Define model ######
+    logger.info('Constructing Model..')
+    model = getattr(models, args.model)(config)#initialize the model
+    ckpt = f"./trained_model/step{args.reload_from}.h5"
+    model.load_state_dict(torch.load(ckpt, map_location=device))
+    model.eval()
+    data_path = args.data_path+args.dataset+'/'
+    
+    vocab_desc = load_dict(data_path+config['vocab_desc'])
+    codebase = load_codebase(data_path+config['use_codebase'], args.chunk_size)
+    codevecs = load_codevecs(data_path+config['use_codevecs'], args.chunk_size)
+    assert len(codebase)==len(codevecs), \
+         "inconsistent number of chunks, check whether the specified files for codebase and code vectors are correct!"
+
+    run(HTTPServer, NaccCodeSearchHandler)
